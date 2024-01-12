075a6e698e70bbb6afc27767778a89abe0c0ee35
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2024-01-12 09:25:22 +0100

    add canonical

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -61,6 +61,10 @@ export default defineNuxtConfig({
 					href: 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
 					media: 'print', 
 					onload: 'this.media="all"'
+				},
+				{
+					rel: 'canonical',
+					href: 'https://8bitjelly.com'
 				}
 			],
 			script: [{
