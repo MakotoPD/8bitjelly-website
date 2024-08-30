aaebe587b4ac36f664954fbc7883ce8fb9fb0602
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2024-08-31 01:30:55 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,7 +75,7 @@ export default defineNuxtConfig({
 				{ defer: true, src: 'https://cloud.umami.is/script.js', "data-website-id": "f5e6e1c2-8bca-41c2-b6e3-c0f66d215ddf"},
 				{
 					type: 'application/ld+json',
-					children: '{"@context": "https://schema.org","@type": "Corporation","name": "8BitJelly","alternateName": "8bitjelly","url": "https://8bitjelly.com/","logo": "https://8bitjelly.com/_vercel/image?url=%2Flogo_white.svg&w=1536&q=40","sameAs": ["https://www.facebook.com/8bitjelly","https://www.github.com/8bitjelly","https://www.8bitjelly.com","https://www.instagram.com/8bitjelly"],"keywords": "gamedev, gamedev polish, gamedev poland, game studio, games, game, developers, developer, 8bit, 8bitjelly, unity, unity game development, unity game, indie game, independent game, unity games, game making, fromsoftware games, indie game dev, digital extreme, unity game dev"}'
+					children: '{"@context": "https://schema.org","@type": "Corporation","name": "8BitJelly","alternateName": "8bitjelly","url": "https://8bitjelly.com/","logo": "https://8bitjelly.com/logo_white.svg","sameAs": ["https://www.facebook.com/8bitjelly","https://www.github.com/8bitjelly","https://www.8bitjelly.com","https://www.instagram.com/8bitjelly"],"keywords": "gamedev, gamedev polish, gamedev poland, game studio, games, game, developers, developer, 8bit, 8bitjelly, unity, unity game development, unity game, indie game, independent game, unity games, game making, fromsoftware games, indie game dev, digital extreme, unity game dev"}'
 				}
 			
 			]
