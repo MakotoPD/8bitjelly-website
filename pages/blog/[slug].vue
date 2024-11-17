beed05352f88c8f5bd00e3d6d88cb8a65641cfaf
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2024-11-17 20:31:20 +0100

    Update [slug].vue

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -11,35 +11,7 @@
 
 			<div v-for="wpis in blog.data">
 
-				<div v-for="text in wpis.attributes.zawartosc">
-
-					<h1 class="text-6xl my-4" v-if="text.type == 'heading' && text.level == 1"> <span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }" >{{ x.text }}</span></h1>
-					<h2 class="text-5xl my-4" v-if="text.type == 'heading' && text.level == 2"><span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }">{{ x.text }}</span></h2>
-					<h3 class="text-4xl my-4" v-if="text.type == 'heading' && text.level == 3"><span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }">{{ x.text }}</span></h3>
-					<h4 class="text-3xl my-4" v-if="text.type == 'heading' && text.level == 4"><span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }">{{ x.text }}</span></h4>
-					<h5 class="text-2xl my-4" v-if="text.type == 'heading' && text.level == 5"><span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }">{{ x.text }}</span></h5>
-					<h6 class="text-xl my-4" v-if="text.type == 'heading' && text.level == 6"><span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }">{{ x.text }}</span></h6>
-
-					<code v-if="text.type == 'code'" class="px-1 py-0.5 bg-darker/20 rounded-md"><span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }">{{ x.text }}</span></code>
-
-					<p class="text-md" v-if="text.type == 'paragraph'"><span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }">{{ x.text }}</span></p>
-
-					<p v-if="text.type == 'quote'" class="border-l-4 border-l-darker/20 pl-2"><span v-for="x in text.children" :class="{ 'font-semibold': x.bold, 'italic': x.italic, 'underline': x.underline, 'line-through': x.strikethrough }">{{ x.text }}</span></p>
-
-
-					<ul class="list list-disc list-inside" v-if="text.type == 'list' && text.format == 'unordered'">
-						<li v-for="x in text.children">{{ x.children[0].text }}</li>
-					</ul>
-
-					<ol class="list list-decimal list-inside" v-if="text.type == 'list' && text.format == 'ordered'">
-						<li v-for="x in text.children">{{ x.children[0].text }}</li>
-					</ol>
-
-					<img class="my-2 rounded-xl" v-if="text.type == 'image'" :src="text.image.url" :alt="text.image.name">
-
-
-
-				</div>
+				<div class="blogpost" v-html="wpis.attributes.blog"></div>
 
 				<p class="mt-8 text-sm bg-primary/30 px-2 py-1 rounded-lg border border-darker/30 w-fit">{{ wpis.attributes.autor }}, {{ wpis.attributes.publishedAt.split('T')[0] }}</p>
 
@@ -50,6 +22,40 @@
 		<Foter/>
 	</div>
 </template>
+
+<style>
+.blogpost h1{
+	@apply text-5xl my-4 font-semibold
+}
+.blogpost h2{
+	@apply text-4xl my-4
+}
+.blogpost h3{
+	@apply text-3xl my-4
+}
+.blogpost h4{
+	@apply text-2xl my-4
+}
+.blogpost p{
+	@apply my-4
+}
+
+@media screen and (max-width:800px) {
+	.blogpost h1{
+		@apply text-3xl
+	}
+	.blogpost h2{
+		@apply text-2xl
+	}
+	.blogpost h3{
+		@apply text-xl
+	}
+	.blogpost h4{
+		@apply text-lg
+	}
+}
+
+</style>
 
 <script setup lang="ts">
 const { slug } = useRoute().params;
